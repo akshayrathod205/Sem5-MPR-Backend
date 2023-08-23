92db9b0991c4f1975e94236f24294a035870a578
Akshay Rathod <kr.akshay234@gmail.com>
2023-08-23 20:25:00 +0530

    added authRouter

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db/connect");
+const authRouter = require("./routes/auth");
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", authRouter);
 
 const port = process.env.PORT || 5000;
 
