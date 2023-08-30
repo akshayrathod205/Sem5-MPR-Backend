ee83d9514fb2d89a73fa90c16de48667b16e7461
Akshay Rathod <kr.akshay234@gmail.com>
2023-08-30 22:48:46 +0530

    added routes

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db/connect");
 const authRouter = require("./routes/auth");
+const dashboardRouter = require("./routes/dashboard");
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", authRouter);
+app.use("api/v1/auth", authRouter);
+app.use("api/v1/dashboard", dashboardRouter);
 
 const port = process.env.PORT || 5000;
 
