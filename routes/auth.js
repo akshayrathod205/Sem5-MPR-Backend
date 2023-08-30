d1f98840e2574dff009bddf03bff09fc7d56565e
Akshay Rathod <kr.akshay234@gmail.com>
2023-08-30 23:38:29 +0530

    minor error resoved

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { login } = require("../controllers/auth");
+const { consumerSignup, consumerLogin } = require("../controllers/auth");
 
-router.route("consumer/signup").post(consumerSignup);
-router.route("consumer/login").post(consumerLogin);
-router.route("admin/login").post(adminLogin);
+router.route("/consumer/signup").post(consumerSignup);
+router.route("/consumer/login").post(consumerLogin);
+// router.route("admin/login").post(adminLogin);
 
 module.exports = router;
