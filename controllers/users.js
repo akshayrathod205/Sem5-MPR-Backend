00a165190805d936b465830f5135ef2eed072feb
Akshay Rathod <kr.akshay234@gmail.com>
2023-10-08 11:02:04 +0530

    profile done

---
+++ controllers/users.js
@@ -0,0 +1,27 @@
+const User = require("../models/User");
+
+const getUser = async (req, res) => {
+  try {
+    const loggedInUserId = req.user.payload.userId;
+    const loggedInUser = await User.findById(loggedInUserId);
+    res.status(200).json({ user: loggedInUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const loggedInUserId = req.user.payload.userId;
+    const updatedUser = await User.findByIdAndUpdate(loggedInUserId, req.body, {
+      new: true,
+    });
+    res.status(200).json({ user: updatedUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getUser, updateUser };
